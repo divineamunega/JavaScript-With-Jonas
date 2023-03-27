7da23a14d7254ae241470b9112fe76757b58c18f
Divine Amunega <divineamunega@gmail.com>
2023-03-27 07:46:54 +0100

    modified:   07-Pig-Game/starter/script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -5,7 +5,35 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+const currentScore0 = document.getElementById('current--0');
+const currentScore1 = document.getElementById('current--1');
+let currentScore = 0;
+
 // Setting the initial Values of the game
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// Rolling Dice Functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generate a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // 2 Display the dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //3. Check if rolled 1; If true switch to the next player
+  if (dice !== 1) {
+    // Add dice to current Score
+    currentScore += dice;
+    currentScore0.textContent = currentScore; // CHANGE LATER
+  } else {
+    // Switch to next player
+  }
+});
