9d5ef3a56816f28aae736881ca02850e8ffec439
Divine Amunega <divineamunega@gmail.com>
2023-04-05 00:01:16 +0100

    modified:   07-Pig-Game/starter/script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -17,11 +17,11 @@ const btnHold = document.querySelector('.btn--hold');
 const currentScore0 = document.getElementById('current--0');
 const currentScore1 = document.getElementById('current--1');
 
-let scores,activeValue,currentScore,playing;
+let scores, activeValue, currentScore, playing;
 const name0 = document.querySelector('#name--0');
 const name1 = document.querySelector('#name--1');
 
-const init = function(){
+const init = function () {
   playing = true;
   scores = [0, 0];
   currentScore = 0;
@@ -33,16 +33,15 @@ const init = function(){
   for (let i = 0; i < player.length; i++) {
     score[i].textContent = scores[i];
   }
-  
+
   activeValue = 0;
   player[0].classList.add('player--active');
   score0El.textContent = 0;
   score1El.textContent = 0;
-  diceEl.classList.add('hidden')
+  diceEl.classList.add('hidden');
+};
 
-}
-
-init()
+init();
 
 // Switch Player Functionality
 const switchPlayer = function () {
@@ -87,12 +86,11 @@ btnHold.addEventListener('click', function () {
       playing = false;
       player[activeValue].classList.remove('player--active');
       player[activeValue].classList.add('player--winner');
-      diceEl.classList.add('hidden')
+      diceEl.classList.add('hidden');
     } else {
       switchPlayer();
     }
   }
 });
-
 
 btnNew.addEventListener('click', init);
