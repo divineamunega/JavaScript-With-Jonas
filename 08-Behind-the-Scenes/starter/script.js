9d7d071e40d2a7d74570cab3174814d80e762d91
Divine Amunega <divineamunega@gmail.com>
2023-04-05 01:48:30 +0100

    modified:   08-Behind-the-Scenes/starter/script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
-
-/*------------------------ Scoping In Practice -----------------------*/ 
-
+/*------------------------ Scoping In Practice -----------------------*/
 
 /*  function calcAge(birthYear) {
   const age = 2037 - birthYear;
@@ -44,7 +42,47 @@ const firstName = `Jonas`;
 calcAge(1991);
 
 // console.log(printAge()); Reference Error
-// console.log(age); Reference Error */ 
+// console.log(age); Reference Error */
+
+// Hoisting with Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+// console.log(divine);
+
+var me = `Divine`;
+let job = `😭😭`;
+const year = 2007;
+
+// Hoisting with functions
+
+console.log(addDecl(10, 2));
+// console.log(adddExpr(10, 2));
+// console.log(adddExpr(10, 2));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+var adddExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
 
 
 
+if(!numProducts) deleteShoppingCart();
+var numProducts = 10;
+
+function deleteShoppingCart(){
+    console.log(`All Products deleted`)
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x ===  window.x);
+console.log(y === window.y);
+console.log(z === window.z);
