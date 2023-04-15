f5996896ca118ad6e5de34b89e1964b7854b8f78
Divine Amunega <divineamunega@gmail.com>
2023-04-15 10:31:31 +0100

    modified:   09-Data-Structures-Operators/starter/script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -54,7 +54,69 @@ const restaurant = {
   },
 };
 
+// Coding Challenge 1
 
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+const [players1,players2] = game.players;
+
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+const players1Final = [...players1,`Thiago`,`Coutinho`, `Periscic`]
+console.log(players1Final);
+
+const {team1,x:draw,team2} = game.odds;
+console.log(team1,draw,team2);
+
+const printGoals =  function(...players){
+  console.log(`The total goals scored was ${players.length}`);
+  for(let i = 0; i < players.length; i++)console.log(players[i]);
+}
+
+team1 < team2 && console.log(`Team 1 is more likely to win`);
+team1 > team2 && console.log(`Team 2 is more likely to win`);
 
 /*----------Logical Assignment Operators------------------*/
 /*
