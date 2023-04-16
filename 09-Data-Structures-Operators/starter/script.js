3ef98098a668142cab1e24d2c2e0ca14bbf82359
Divine Amunega <divineamunega@gmail.com>
2023-04-16 21:05:12 +0100

    modified:   09-Data-Structures-Operators/starter/script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -96,6 +96,29 @@ const game = {
   },
 };
 
+
+/**-------------Looping Objects--------------------- */
+
+// property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+let openStr = (`We open ${properties.length} days a week`);
+for(const day of Object.keys(openingHours)) {
+  openStr+=` ${day},`;
+}
+console.log(openStr);
+
+// property VAKUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire Object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for(const [key, {open,close}] of entries){
+  console.log(`On ${key} we open at ${open} and close at ${close}.`);
+}
 /**---------------Optional Chaining------------------ */
 /*if (restaurant.openingHours && restaurant.openingHours.mon) {
   console.log(restaurant.openingHours.mon.open);
