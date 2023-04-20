ce800a59601dd1837a61ed8943eef24ed9619fd7
Divine Amunega <divineamunega@gmail.com>
2023-04-20 16:46:51 +0100

    modified:   09-Data-Structures-Operators/starter/script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -110,7 +110,7 @@ const gameEvents = new Map([
 ]);
 
 // Strings Methods Practice
-
+/*
 const getCode = str => str.slice(0,3).toUpperCase();
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -125,6 +125,7 @@ for (const [i, flight] of flightsArr.entries()) {
   } ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);  
   console.log(output);
 }
+*/
 
 /** ----------------------- Coding Challenge 4-------------------- */
 /*
