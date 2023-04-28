7a99b8306529d15935d8d9a32321abc534e1350d
Divine Amunega <divineamunega@gmail.com>
2023-04-29 00:17:50 +0100

    modified:   11-Arrays-Bankist/starter/script.js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const displayMovements = function (movements) {
           <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-          <div class="movements__value">${Math.abs(mov)}</div>
+          <div class="movements__value">${Math.abs(mov)} €</div>
         </div>`;
 
     containerMovements.insertAdjacentHTML(`afterbegin`, html);
@@ -91,12 +91,33 @@ const createUserName = accs => {
 
 createUserName(accounts);
 
-const calcDisplayBalance = movements => {
-  const balance = movements.reduce((acc, mov) => acc + mov);
-  labelBalance.textContent = `${balance} €`;
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes} €`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter(int => int >= 1)
+    .reduce((acc, int) => acc + int, 0);
+    labelSumInterest.textContent = `${interest} €`
 };
 
-calcDisplayBalance(account1.movements);
+calcDisplaySummary(account1.movements);
+
+const calcDisplayBalance = movements => {
+  const balance = movements.reduce((acc, mov) => acc + mov);
+  labelBalance.textContent = `${balance}€`;
+};
+
+calcDisplayBalance(accounts[0].movements);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -277,8 +298,8 @@ console.log(movmentDescriptions);
 
 ///////////////////////////////////
 // THe Reduce Method
-/*
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+/*
 
 
 // Accumulator => Snowball
@@ -326,3 +347,10 @@ const calcAverageHumanAge = function (arr) {
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 */
+const eurToUsd = 1.1;
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+
+console.log(totalDepositsUSD);
