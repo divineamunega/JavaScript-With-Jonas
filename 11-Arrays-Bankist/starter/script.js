1c036c7ba4088583addc948235c485efc9fbddf4
Divine Amunega <divineamunega@gmail.com>
2023-04-28 22:22:39 +0100

    modified:   11-Arrays-Bankist/starter/script.js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -92,9 +92,14 @@ const createUserName = (accs) => {
 
 createUserName(accounts);
 
+const calcDisplayBalance = movements => {
+  const balance = movements.reduce((acc, mov) => acc + mov);
+  labelBalance.textContent = `${balance} €`;
+};
 
 
-// console.log(containerMovements.innerHTML);
+calcDisplayBalance(account1.movements);
+
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -284,3 +289,14 @@ for(const mov of movements){
   balance2 += mov;
 }
 console.log(balance2);
+
+// Maximum value
+const max = movements.reduce((acc,mov) => {
+  if(acc > mov){
+    return acc;
+  }else{
+    return mov;
+  }
+}, movements[0])
+
+console.log(max);
