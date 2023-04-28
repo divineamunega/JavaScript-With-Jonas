b54111a7414710fd2f25cbf247b44a989742395c
Divine Amunega <divineamunega@gmail.com>
2023-04-29 00:23:23 +0100

    modified:   11-Arrays-Bankist/starter/script.js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -347,6 +347,9 @@ const calcAverageHumanAge = function (arr) {
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 */
+
+
+/*
 const eurToUsd = 1.1;
 const totalDepositsUSD = movements
   .filter(mov => mov > 0)
@@ -354,3 +357,26 @@ const totalDepositsUSD = movements
   .reduce((acc, mov) => acc + mov, 0);
 
 console.log(totalDepositsUSD);
+*/
+
+//////////////////////////////
+// CODING CHALLENGE 3
+
+/*
+const calcAverageHumanAge = function (arr) {
+  const humanAge = arr
+    .map(dogAge => {
+      const humanAge = dogAge <= 2 ? dogAge * 2 : 176 + dogAge * 4;
+      return humanAge;
+    })
+    .filter(humanAge => humanAge > 18)
+    .reduce((acc, val, i, el) => {
+      return acc + val / el.length;
+    }, 0);
+
+  return humanAge;
+};
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+*/
