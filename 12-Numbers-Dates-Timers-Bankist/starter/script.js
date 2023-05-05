37ba5fce1dd8a4b0c18c13970a31b0011bc7ea91
Divine Amunega <divineamunega@gmail.com>
2023-05-05 20:03:44 +0100

    Operations With Dates Lecture 177

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -21,9 +21,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2023-05-02T17:01:17.194Z',
+    '2023-05-04T23:36:17.929Z',
+    '2023-05-05T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -81,6 +81,25 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if (daysPassed === 0) return `Today`;
+  if (daysPassed === 1) return `Yesterday`;
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+  const [day, month, year] = [
+    `${date.getDate()}`.padStart(2, '0'),
+    `${date.getMonth() + 1}`.padStart(2, '0'),
+    date.getFullYear(),
+  ];
+  return `${day}/${month}/${year}`;
+};
+
 const displayMovements = function (acc, sort) {
   containerMovements.innerHTML = '';
 
@@ -93,13 +112,7 @@ const displayMovements = function (acc, sort) {
 
     const date = new Date(acc.movementsDates[i]);
 
-    const [day, month, year] = [
-      `${date.getDate()}`.padStart(2, '0'),
-      `${date.getMonth() + 1}`.padStart(2, '0'),
-      date.getFullYear(),
-    ];
-
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
@@ -452,9 +465,20 @@ console.log(future.getSeconds());
 console.log(future.getMilliseconds());
 console.log(future.toISOString());
 console.log(future.getTime());
-
+ 
 console.log(Date.now());
 
 future.setFullYear(2040)
 console.log(future);
+*/
+
+//////////////////////////////////////////
+// Operations With Dates
+/*
+const future = new Date(2037, 10, 19, 15, 23, 5);
+console.log(+future);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2023, 4, 5));
+
+console.log(days1);
 */
