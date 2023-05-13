0e85baf8ef9d22a597d5f0d7252484f0f28a06a0
Divine Amunega <divineamunega@gmail.com>
2023-05-13 10:13:41 +0100

    Style, Attributes and Classes

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -59,3 +59,44 @@ header.after(message);
 document.querySelector(`.btn--close-cookie`).addEventListener(`click`, function(){
   message.remove();
 })
+
+// Styles
+message.style.backgroundColor  = `#37383d`;
+message.style.width = `100%`;
+
+console.log(message.style.height);
+
+console.log(getComputedStyle(message).color);
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height,10) + 30 + `px`;    
+ 
+document.documentElement.style.setProperty(`--color-primary`, `orangered`);
+
+
+// Atributes
+const logo =  document.querySelector(`.nav__logo`);
+console.log(logo.alt);
+console.log(logo.className);
+console.log(logo.getAttribute(`designer`));
+logo.alt = `Beautiful Minimalist Logo`;
+logo.setAttribute(`company`, `Bankist`);
+
+console.log(logo.src);
+console.log(logo.getAttribute(`src`));
+
+const link =  document.querySelector(`.nav__link--btn`);
+console.log(link.href);
+console.log(link.getAttribute(`href`));
+
+// Data Attributes
+console.log(logo.dataset.versionNumber)
+
+// Classes
+logo.classList.add(`c`,`j`);
+logo.classList.remove(`c`,`j`);
+logo.classList.toggle(`c`);
+logo.classList.contains(`c`); // Not Includes
+
+
+// Dont use 
+logo.className = `Jonas`;
