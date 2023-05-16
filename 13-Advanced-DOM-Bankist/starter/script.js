381cf584e15d06ce9199e2434f7b5051d2cf5bb2
Divine Amunega <divineamunega@gmail.com>
2023-05-16 13:35:57 +0100

    Implementing a sticky Navigation

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -103,10 +103,20 @@ const handleHover = function (e) {
 };
 
 // Menu Fade Animation
-nav.addEventListener(`mouseover`,handleHover.bind(0.5)); 
+nav.addEventListener(`mouseover`, handleHover.bind(0.5));
 nav.addEventListener(`mouseout`, handleHover.bind(1.0));
 
+// Sticky Navigation
+const section1 = document.querySelector(`#section--1`);
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
 
+window.addEventListener(`scroll`, function (e) {
+  console.log(window.scrollY);
+
+  if (this.window.scrollY > initialCoords.top) nav.classList.add(`sticky`);
+  else nav.classList.remove(`sticky`);
+});
 
 //////////////////////////////
 // Creating Selecting and deleting elements
