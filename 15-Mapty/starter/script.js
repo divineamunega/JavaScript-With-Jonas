97c26fa660dd4c0a10d2b52b12092b67ad9b999e
Divine Amunega <divineamunega@gmail.com>
2023-06-30 14:00:22 +0100

    Creating Workouts Objects

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -23,9 +23,55 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = new Date().getTime() + ``;
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // km
+    this.duration = duration; // min
+  }
+}
+
+class Running extends Workout {
+  type = `running`;
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  type = `cycling`;
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([12.3, 45], 5.2, 24, 178);
+// const cycle1 = new Running([12.3, 45], 27, 59, 528);
+// console.log(run1, cycle1);
+
+///////////////////////////////////
+// The App Class
+//////////////////////////////////////////////////////////
 class App {
   #map;
   #mapEvent;
+  #workouts = [];
 
   constructor() {
     this.#getPosition();
@@ -73,16 +119,63 @@ class App {
   }
 
   #newWorkout(e) {
-    // Clear Input Fields
+    const validInputs = (...input) =>
+      input.every(inp => Number.isFinite(inp) && inp > 0);
+    e.preventDefault();
+
+    // Get Data From Form
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+    const coords = [this.#mapEvent.latlng.lat, this.#mapEvent.latlng.lng];
+    let workout;
+
+    // If the workout is running --> Running Object
+    if (type === `running`) {
+      // Check If data is valid
+      const cadence = +inputCadence.value;
+      if (!validInputs(cadence, distance, duration))
+        return alert('Inputs have to be positive numbers');
+
+      // Create New Workout
+      workout = new Running(coords, distance, duration, cadence);
+    }
+
+    // If the workout is cycling --> Cycling Object
+    if (type === `cycling`) {
+      const elevation = +inputElevation.value;
+
+      // Check if data is valid
+      if (!validInputs(distance, duration) || !Number.isFinite(elevation))
+        return alert('Inputs have to be positive numbers');
+
+      // Create New Workout
+      workout = new Cycling(coords, distance, duration, elevation);
+    }
+
+    // Add new object to workout Array
+    this.#workouts.push(workout);
+    console.log(this.#workouts);
+
+    // Render workout on the map as a marker
+    this.#renderWorkoutMarker(workout);
+
+    // Render workout on list
+    this.#renderWorkout(workout);
+
+    // Hide form + Clear input fields
     inputCadence.value =
       inputCadence.value =
       inputDuration.value =
       inputElevation.value =
+      inputDistance.value =
         ``;
-    // Display the Marker
-    e.preventDefault();
-    const coords = [this.#mapEvent.latlng.lat, this.#mapEvent.latlng.lng];
-    L.marker(coords, { riseOnHover: true })
+    form.classList.add(`hidden`);
+  }
+
+  // Render Workout Marker Function
+  #renderWorkoutMarker(workout) {
+    L.marker(workout.coords, { riseOnHover: true })
       .addTo(this.#map)
       .bindPopup(
         L.popup({
@@ -90,25 +183,23 @@ class App {
           minWidth: 100,
           autoClose: false,
           closeOnClick: false,
-          className: 'running-popup',
+          className: `${workout.type}-popup`,
         })
       )
-      .setPopupContent(`Workout`)
+      .setPopupContent(`${workout.distance}`)
       .openPopup();
   }
+
+  // RenderWorkout
+  #renderWorkout() {}
 }
 const app = new App();
 console.log(app);
+// const dive = [1,2,3,].every(ind => Number.isFinite(ind) && ind > 0);
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (pos) {
-      console.log(pos);
-    },
-    function (err) {
-      console.log(`Error getting your location ${err.message}`);
-    }
-  );
-} else {
-  console.log('Geolocation is not supported by this browser.');
-}
+// console.log(dive);
+
+// const validInputs = (...input) =>
+//       input.every(inp => Number.isFinite(inp) && inp > 0);
+
+// console.log(validInputs(1,2,3,''));
